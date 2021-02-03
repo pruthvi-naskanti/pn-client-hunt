f686f5d16827f6d47ed2de6928108844fe5dce14
pruthvi-naskanti <pruthvinaskanti@gmail.com>
2021-02-02 21:30:04 -0600

    commit

--- init-locations.js
+++ init-locations.js
@@ -1,11 +1,13 @@
 export default [{
         Name: 'Lacelede Shipping',
         Latitude: 40.356121121713656,
-        Longitude: -94.88189112967979
+        Longitude: -94.88189112967979,
     },
     {
-        Name: 'Library',
-        Latitude: 40.35351493594575,
-        Longitude: -94.88361126157204
+        Name: 'C HALL',
+        North: 40.35085725998568,
+        South: 40.35085725998568,
+        East: -94.88238755790566,
+        West: -94.88238755790566,
     }
 ];
